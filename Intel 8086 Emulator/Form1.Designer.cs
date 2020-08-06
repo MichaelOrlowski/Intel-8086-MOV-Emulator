b387f7fc2084d019859671a06f76054327c7ae54
Michał Orłowski <michal.orlowski@microsoft.wsei.edu.pl>
2020-08-06 20:14:57 +0200

    Inital Comit

---
+++ Intel 8086 Emulator/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Intel_8086_Emulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxAX = new System.Windows.Forms.TextBox();
+            this.textBoxBX = new System.Windows.Forms.TextBox();
+            this.textBoxCX = new System.Windows.Forms.TextBox();
+            this.textBoxDX = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.RejestrBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ŹRÓDŁO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DOCELOWY:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AX",
+            "BX",
+            "CX",
+            "DX"});
+            this.comboBox1.Location = new System.Drawing.Point(51, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DisplayMember = "AX";
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "AX",
+            "BX",
+            "CX",
+            "DX"});
+            this.comboBox2.Location = new System.Drawing.Point(51, 123);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(244, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "AX";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "BX";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(244, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "CX";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(244, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "DX";
+            // 
+            // textBoxAX
+            // 
+            this.textBoxAX.Location = new System.Drawing.Point(272, 46);
+            this.textBoxAX.Name = "textBoxAX";
+            this.textBoxAX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAX.TabIndex = 8;
+            // 
+            // textBoxBX
+            // 
+            this.textBoxBX.Location = new System.Drawing.Point(272, 72);
+            this.textBoxBX.Name = "textBoxBX";
+            this.textBoxBX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBX.TabIndex = 9;
+            // 
+            // textBoxCX
+            // 
+            this.textBoxCX.Location = new System.Drawing.Point(272, 98);
+            this.textBoxCX.Name = "textBoxCX";
+            this.textBoxCX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCX.TabIndex = 10;
+            // 
+            // textBoxDX
+            // 
+            this.textBoxDX.Location = new System.Drawing.Point(272, 124);
+            this.textBoxDX.Name = "textBoxDX";
+            this.textBoxDX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDX.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Wykonaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Wyczyść Rejestr";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Intel_8086_Emulator.Properties.Resources.KL_Intel_TD8086;
+            this.pictureBox1.Location = new System.Drawing.Point(434, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(48, 225);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "REJESTR:";
+            // 
+            // RejestrBox
+            // 
+            this.RejestrBox.AcceptsReturn = true;
+            this.RejestrBox.AcceptsTab = true;
+            this.RejestrBox.Location = new System.Drawing.Point(51, 241);
+            this.RejestrBox.Multiline = true;
+            this.RejestrBox.Name = "RejestrBox";
+            this.RejestrBox.ReadOnly = true;
+            this.RejestrBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.RejestrBox.Size = new System.Drawing.Size(460, 89);
+            this.RejestrBox.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RejestrBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxDX);
+            this.Controls.Add(this.textBoxCX);
+            this.Controls.Add(this.textBoxBX);
+            this.Controls.Add(this.textBoxAX);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "INTEL 8086 MOV EMULATOR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxAX;
+        private System.Windows.Forms.TextBox textBoxBX;
+        private System.Windows.Forms.TextBox textBoxCX;
+        private System.Windows.Forms.TextBox textBoxDX;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox RejestrBox;
+    }
+}
+
---
+++ Intel 8086 Emulator/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intel_8086_Emulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeValue(string rejestr1, string rejestr2)
+        {
+            if(rejestr1 == rejestr2)
+            {
+                RejestrBox.Text += "Nie można przypisać wartości do tego samego rejestru \r\nWybierz różne rejestry! \r\n";
+
+            }
+            else
+            {
+                int AfterParseRejestr1;
+
+                var TextBoxRejestr1 = Controls.Find("TextBox" + rejestr1, true);
+                TextBox TextBoxRejestr1_1 = (TextBox)TextBoxRejestr1[0] as TextBox;
+            
+
+                var TextBoxRejestr2 = Controls.Find("TextBox" + rejestr2, true);
+                TextBox TextBoxRejestr2_1 = (TextBox)TextBoxRejestr2[0] as TextBox;
+
+                if (int.TryParse(TextBoxRejestr1_1.Text, out AfterParseRejestr1))
+                {
+                    TextBoxRejestr2_1.Text = TextBoxRejestr1_1.Text;
+                    RejestrBox.Text += "MOV  " + rejestr1 + "," + rejestr2 + "     // " + rejestr2 +  " przyjmuję wartość " + TextBoxRejestr1_1.Text +  "  \r\n";
+                }
+                else
+                {
+                    MessageBox.Show("Dane muszą być liczbą");
+                    return;
+                }
+
+                
+            }
+
+        }
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem)
+            {
+                case "AX":
+                    switch (comboBox2.SelectedItem)
+                    {
+                        case "AX":
+                            ChangeValue("AX", "AX");
+                            break;
+
+                        case "BX":
+                            ChangeValue("AX", "BX");
+                            break;
+
+                        case "CX":
+                            ChangeValue("AX", "CX");
+                            break;
+
+                        case "DX":
+                            ChangeValue("AX", "DX");
+                            break;
+
+                        default:
+                            RejestrBox.Text += "Błąd przypisywania wartości. Pola Są puste";
+                            break;
+
+                    }
+                    break;
+
+                case "BX":
+                    switch (comboBox2.SelectedItem)
+                    {
+                        case "AX":
+                            ChangeValue("BX", "AX");
+                            break;
+
+                        case "BX":
+                            ChangeValue("BX", "BX");
+                            break;
+
+                        case "CX":
+                            ChangeValue("BX", "CX");
+                            break;
+
+                        case "DX":
+                            ChangeValue("BX", "DX");
+                            break;
+
+                        default:
+                            RejestrBox.Text += "Błąd przypisywania wartości. Pola Są puste";
+                            break;
+
+                    }
+                    break;
+
+                case "CX":
+                    switch (comboBox2.SelectedItem)
+                    {
+                        case "AX":
+                            ChangeValue("CX", "AX");
+                            break;
+
+                        case "BX":
+                            ChangeValue("CX", "BX");
+                            break;
+
+                        case "CX":
+                            ChangeValue("CX", "CX");
+                            break;
+
+                        case "DX":
+                            ChangeValue("CX", "DX");
+                            break;
+
+                        default:
+                            RejestrBox.Text += "Błąd przypisywania wartości. Pola Są puste";
+                            break;
+
+                    }
+                    break;
+
+                case "DX":
+                    switch (comboBox2.SelectedItem)
+                    {
+                        case "AX":
+                            ChangeValue("DX", "AX");
+                            break;
+
+                        case "BX":
+                            ChangeValue("DX", "BX");
+                            break;
+
+                        case "CX":
+                            ChangeValue("DX", "CX");
+                            break;
+
+                        case "DX":
+                            ChangeValue("DX", "DX");
+                            break;
+
+                        default:
+                            RejestrBox.Text += "Błąd przypisywania wartości. Pola Są puste";
+                            break;
+
+                    }
+                    break;
+
+                default:
+                    RejestrBox.Text += "Błąd przypisywania wartości. Pola Są puste";
+                    break;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RejestrBox.Text = "";
+        }
+
+    }
+}
